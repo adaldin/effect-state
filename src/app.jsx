daa73322cfb27fae9b5f915e98a4c6f0b5ca9c31
Hiena <aledaldin@gmail.com>
2022-12-08 16:36:53 +0100

    erased comments from PR

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,6 @@
 import { ShapeForm } from './components/organisms/ShapeForm'
 
-// Al tener toda la lógica en un mismo componente se vuelve dificil de leer.
-// Crearia los siguientes componentes en atoms: Label,
-// InputNumber, InputColor, Triangle, Square y Circle.
-// Crearia componentes/hooks para agrupar y reutilizar lógica.
 export function App() {
-  // Puedes usar un state para cada valor, aunque esta bien
-
   return (
     <div>
       <h4>Ejercicio useEffect useState.</h4>
