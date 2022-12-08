50ff378b050879e882b33a7631a8c2ce51163e8c
Hiena <aledaldin@gmail.com>
2022-12-08 16:31:36 +0100

    fixed estructure and components

--- src/components/organisms/ShapeForm.jsx
+++ src/components/organisms/ShapeForm.jsx
@@ -1,115 +1,88 @@
 import { Triangle, Circle, Square } from '../atoms'
 import { InputGroupColor, InputGroupNumber } from '../molecules/index'
 import { useState, useEffect } from 'react'
-import { useAttribute } from '../../hooks/useAttribute'
 
 export function ShapeForm() {
-  const [triangle, setTriangle] = useState({
-    triangleColor: 'black',
-    triangleSize: '100',
-  })
-  const [circle, setCircle] = useState({
-    circleColor: 'red',
-    circleSize: '50',
-  })
-  const [square, setSquare] = useState({
-    squareColor: 'blue',
-    squareSize: '60',
-  })
+  const [triangleColor, setTriangleColor] = useState('black')
+  const [triangleSize, setTriangleSize] = useState('30')
+  const [circleColor, setCircleColor] = useState('red')
+  const [circleSize, setCircleSize] = useState('40')
+  const [squareColor, setSquareColor] = useState('orange')
+  const [squareSize, setSquareSize] = useState('50')
 
   useEffect(() => {
-    if (
-      circle.circleSize === triangle.triangleSize &&
-      circle.circleSize === square.squareSize
-    ) {
+    if (circleSize === triangleSize && circleSize === squareSize) {
       alert('Shape sizes are equal')
-    } else if (
-      circle.circleColor === triangle.triangleColor &&
-      circle.circleColor === square.squareColor
-    ) {
+    } else if (circleColor === triangleColor && circleColor === squareColor) {
       alert('Shape colors are equal')
     }
-  }, [circle, triangle, square])
+  }, [
+    triangleColor,
+    triangleSize,
+    circleColor,
+    circleSize,
+    squareColor,
+    squareSize,
+  ])
 
-  const handleSquare = (e) => {
-    const [color, size] = useAttribute(e)
-  }
-  // const handleSquare = (e) => {
-  //   const { name, value } = e.target
-  //   setSquare((prevShape) => {
-  //     return { ...prevShape, [name]: value }
-  //   })
-  // }
-  const handleTriangle = (e) => {
-    const { name, value } = e.target
-    setTriangle((prevShape) => {
-      return { ...prevShape, [name]: value }
-    })
-  }
-  const handleCircle = (e) => {
-    const { name, value } = e.target
-    setCircle((prevShape) => {
-      return { ...prevShape, [name]: value }
-    })
-  }
   return (
     <form>
       <div>
         <InputGroupColor
-          color={square.squareColor}
-          name="squareColor"
-          onChange={handleSquare}
-          htmlFor="squareColor"
-          label="Square Color"
-          id="squareColor"
-        />
-        <InputGroupNumber
-          size={square.squareSize}
-          name="squareSize"
-          onChange={handleSquare}
-          htmlFor="squareSize"
-          label="Square Size"
-          id="squareSize"
-        />
-        <Square size={square.squareSize} color={square.squareColor} />
-      </div>
-      <div>
-        <InputGroupColor
-          color={triangle.triangleColor}
+          color={triangleColor}
           name="triangleColor"
-          onChange={handleTriangle}
+          onChange={(e) => setTriangleColor(e.target.value)}
           htmlFor="triangleColor"
           label="Triangle Color"
           id="triangleColor"
         />
         <InputGroupNumber
-          size={triangle.triangleSize}
+          size={triangleSize}
           name="triangleSize"
-          onChange={handleTriangle}
+          onChange={(e) => setTriangleSize(e.target.value)}
           htmlFor="triangleSize"
           label="Triangle Size"
           id="triangleSize"
         />
-        <Triangle size={triangle.triangleSize} color={triangle.triangleColor} />
+        <Triangle size={triangleSize} color={triangleColor} />
       </div>
       <div>
         <InputGroupColor
-          color={circle.circleColor}
+          color={circleColor}
           name="circleColor"
-          onChange={handleCircle}
+          onChange={(e) => setCircleColor(e.target.value)}
           htmlFor="circleColor"
           label="Circle Color"
           id="circleColor"
         />
         <InputGroupNumber
-          size={circle.circleSize}
+          size={circleSize}
           name="circleSize"
-          onChange={handleCircle}
+          onChange={(e) => setCircleSize(e.target.value)}
           htmlFor="circleSize"
           label="Circle Size"
           id="circleSize"
         />
-        <Circle size={circle.circleSize} color={circle.circleColor} />
+        <Circle size={circleSize} color={circleColor} />
+      </div>
+      <div>
+        <InputGroupColor
+          color={squareColor}
+          name="squareColor"
+          onChange={(e) => setSquareColor(e.target.value)}
+          htmlFor="squareColor"
+          label="Square Color"
+          id="squareColor"
+        />
+        <InputGroupNumber
+          size={squareSize}
+          name="squareSize"
+          onChange={(e) => setSquareSize(e.target.value)}
+          htmlFor="squareSize"
+          label="Square Size"
+          id="squareSize"
+        />
+        <Square size={squareSize} color={squareColor} />
       </div>
     </form>
   )
